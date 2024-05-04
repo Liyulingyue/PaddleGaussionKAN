ac3c88d3fd3485a3cc8a949e9999f70f18a2776d
liyulingyue <852433440@qq.com>
2024-05-04 21:28:58 +0800

    add Activation

--- KANActivation.py
+++ KANActivation.py
@@ -1,6 +1,17 @@
 import paddle
 
+
 class KANActivation(paddle.nn.Layer):
+    """
+    KANActivation is a Activation function, this function try to fit a polynomial function with normal distribution.
+
+    Args:
+        g (int): Number of Gaussian functions.
+
+    Returns:
+        None
+
+    """
     def __init__(self, g):
         super(KANActivation, self).__init__()
         self.a = paddle.create_parameter(
@@ -20,6 +31,16 @@ class KANActivation(paddle.nn.Layer):
         )
 
     def forward(self, x):
+        """
+        前向传播函数，用于计算 KANActivation 激活函数的输出。
+
+        Args:
+            x (paddle.Tensor): 输入张量，形状为 [batch_size, -1, -1, ...]。
+
+        Returns:
+            paddle.Tensor: 经过激活函数处理后的输出张量，形状和输入形状相同。
+
+        """
         x = paddle.unsqueeze(x, [-1])  # [batch_size, in_channels] -> [batch_size, in_channels, 1]
         x = self.a * paddle.exp(-(x - self.b) ** 2 / (2 * self.c ** 2))  # [batch_size, in_channels, g]
         x = paddle.sum(x, axis=[-1])  # [batch_size, in_channels]
--- README.md
+++ README.md
@@ -4,6 +4,11 @@ This is a PaddlePaddle implementation of KAN.
 
 In this project, a Gasussion Distribution is used to replace the original B-branches.
 
+However, the result of directly using KANLayer to test on MNIST is very poor. Based on this, if KANLayer is changed into an optimizable activation function, the test result on MNIST will be very good. In the experiment, I adopted a three-layer fully connected network with Sigmoid as the activation function, and the convergence speed was very slow. After using KANActivation, the convergence was very fast, and an accuracy of 90% could be achieved in only one epoch.
+
+![](networks_performance.jpg)
+
+
 ## Notes
 The demo use PaddlePaddle(CPU), if you want to use GPU, you need uninstall `paddlepaddle` and install `paddlepaddle-gpu`.
 
